dccdd335f1a53ce5750201e5009267fe72a0189a
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-08-24 10:58:41 -0300

    Requisito 05

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     cardAttr3: '',
     cardRare: 'normal',
     cardTrunfo: false,
+    isSaveButtonDisabled: true,
   };
 
   handle = ({ target }) => {
@@ -20,12 +21,35 @@ class App extends React.Component {
 
     this.setState({
       [name]: value,
+    }, () => {
+      const { cardName, cardDescription, cardImage, cardAttr1, cardAttr2,
+        cardAttr3, cardRare } = this.state;
+
+      if (cardName !== ''
+        && cardDescription !== ''
+        && cardImage !== ''
+        && cardRare !== '') {
+        const sumAttributes = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+        const limitSum = 211;
+        const limitCard = 91;
+        const minimum = -1;
+        if (Number(cardAttr1) > minimum && Number(cardAttr1) < limitCard
+          && Number(cardAttr2) > minimum && Number(cardAttr2) < limitCard
+          && Number(cardAttr3) > minimum && Number(cardAttr3) < limitCard
+          && sumAttributes < limitSum) {
+          this.setState({ isSaveButtonDisabled: false });
+        } else {
+          this.setState({ isSaveButtonDisabled: true });
+        }
+      } else {
+        this.setState({ isSaveButtonDisabled: true });
+      }
     });
   };
 
   render() {
     const { cardName, cardDescription, cardImage, cardAttr1, cardAttr2,
-      cardAttr3, cardRare, cardTrunfo } = this.state;
+      cardAttr3, cardRare, cardTrunfo, isSaveButtonDisabled } = this.state;
 
     return (
       <>
@@ -39,6 +63,7 @@ class App extends React.Component {
           cardAttr3={ cardAttr3 }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
         <Card
           cardName={ cardName }
